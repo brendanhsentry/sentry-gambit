4c4ee58cdeb5b7b9ffe3570dc227b2f02b210fac
Brendan Hy <brendan.hy@sentry.io>
2026-08-19 20:46:14 -0700

    fix: restore chess move animation

--- app/ChessgroundBoard.tsx
+++ app/ChessgroundBoard.tsx
@@ -73,10 +73,7 @@ export function ChessgroundBoard({
       },
       draggable: { showGhost: true },
       premovable: { enabled: false, showDests: true },
-      animation: {
-        enabled: !window.matchMedia("(max-width: 600px)").matches,
-        duration: 180,
-      },
+      animation: { enabled: true, duration: 180 },
       movable: {
         free: false,
         showDests: true,
